75a772c76a958730ea8bff99d5a6250d5a7ab382
春木Haruki <harukiinharu@gmail.com>
2025-07-22 13:01:06 +0800

    replace shonen with asu

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ const lyricNamesMap: { name: string; id: string }[] = [
   { name: '生きていたんだよな', id: 'ikite' },
   { name: '僕が死のうと思ったのは', id: 'boku' },
   { name: '命に嫌われている。', id: 'inochi' },
-  { name: '少年少女', id: 'shonen' },
+  { name: 'アストロノーツ', id: 'asu' },
 ]
 
 export default lyricNamesMap
